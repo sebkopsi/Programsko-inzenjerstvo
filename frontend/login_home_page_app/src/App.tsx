82cd8887303bfab19161a80ead29cb37b8e1b4bd
Vedran Delić <vd002315672@fer.hr>
2025-11-12 22:58:31 +0100

    Added login and home page react app folder

---
+++ frontend/login_home_page_app/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import LoginPage from './components/LoginPage';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<div>404 - Page Not Found</div>} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
