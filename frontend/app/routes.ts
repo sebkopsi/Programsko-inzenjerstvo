c2c6278f29603b9901543e1e9fe3c070285b3f40
Leona Marijanović <leona.marijanovic@fer.hr>
2025-11-12 20:02:33 +0100

    Fix route definition for signup and add user route

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -2,5 +2,6 @@ import { type RouteConfig, index } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("signup", "routes/signup.tsx")
+  route("signup", "routes/signup.tsx"),
+  route("user", "routes/user.tsx")
 ] satisfies RouteConfig;
