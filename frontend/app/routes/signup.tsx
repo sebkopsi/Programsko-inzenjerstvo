c9513aa9e18dd8fa817b99596d93116515e78f67
Leona Marijanović <leona.marijanovic@fer.hr>
2025-11-12 17:21:49 +0100

    Format signup.tsx for consistency

--- frontend/app/routes/signup.tsx
+++ frontend/app/routes/signup.tsx
@@ -1,13 +1,13 @@
 import { SignUpPage } from "~/pages/signUp/SignUpPage";
 
 export function meta({}: { title: string; description: string }) {
-  return [
-    { title: "Sign Up" },
-    { name: "description", content: "Registracija novog korisnika" },
-  ];
+        return [
+                { title: "Sign Up" },
+                { name: "description", content: "Registracija novog korisnika" },
+        ];
 }
 
 export default function SignUp() {
-  return <SignUpPage />;
-
+        return <SignUpPage />;
 }
+
