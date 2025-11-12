463a0063df7a462ed66473eb57e3b2fd317c75fa
Leona Marijanović <leona.marijanovic@fer.hr>
2025-11-12 17:17:04 +0100

    Add files via upload

---
+++ frontend/app/routes/signup.tsx
@@ -0,0 +1,12 @@
+import { SignUpPage } from "~/pages/signUp/SignupPage";
+
+export function meta({}: { title: string; description: string }) {
+  return [
+    { title: "Sign Up" },
+    { name: "description", content: "Registracija novog korisnika" },
+  ];
+}
+
+export default function SignUp() {
+  return <SignUpPage />;
+}
