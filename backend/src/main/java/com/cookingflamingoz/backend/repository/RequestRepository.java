6345ddd177e44fe730bb52c1199d94129b404ebf
Flipi <fpolenus@gmail.com>
2026-01-20 15:49:51 +0100

    admin inbox features(getAll i getReqById)

---
+++ backend/src/main/java/com/cookingflamingoz/backend/repository/RequestRepository.java
@@ -0,0 +1,12 @@
+package com.cookingflamingoz.backend.repository;
+
+import com.cookingflamingoz.backend.model.Request;
+import com.cookingflamingoz.backend.model.RequestSummary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface RequestRepository extends JpaRepository<Request, Integer> {
+
+    List<RequestSummary> findAllProjectedBy();
+
+}
