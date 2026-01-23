85a45ceb3be8c9fded72e8943c8061d4df6ef0c0
Leona Marijanović <leona.marijanovic@fer.hr>
2026-01-23 13:03:15 +0100

    Integrate GlobalError component in ProfilePage
    Add GlobalError component to ProfilePage for error handling.

--- frontend/app/pages/profile/profile.tsx
+++ frontend/app/pages/profile/profile.tsx
@@ -4,8 +4,17 @@ import "../styles/components.css"
 import "../styles/forms.css"
 import UserInfoPage from "./userInfo";
 import UserPrefernces from "./userPrefrences";
+import { GlobalError } from "../components/GlobalError";
 
 export function ProfilePage() {
+  const { errorObject } = useLoaderData();
+
+  return (
+    <GlobalError error={errorObject} />
+      <UserPrefernces />
+    </section>
+  );
+
   return (
       <section id="content">{UserPrefernces()}</section>
   );
