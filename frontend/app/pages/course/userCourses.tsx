81bf3c7ba42389dbdcbb6fd74982f7440c44b23a
Leona Marijanović <leona.marijanovic@fer.hr>
2026-01-23 12:59:09 +0100

    Handle potential undefined data in UserCoursesPage

--- frontend/app/pages/course/userCourses.tsx
+++ frontend/app/pages/course/userCourses.tsx
@@ -20,11 +20,12 @@ export function listCourses(data: any[], empty: any, id: String) {
 
 
 export function UserCoursesPage() {
-  const { userInfoData, allCoursesData, myCoursesData } = useLoaderData();
+  const { userInfoData, allCoursesData, myCoursesData, errorObject } = useLoaderData();
 
   console.debug(allCoursesData)
   return (
     <section id="content">
+      <GlobalError error={errorObject} />
       <h2><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
         <path d="M11.7 2.805a.75.75 0 0 1 .6 0A60.65 60.65 0 0 1 22.83 8.72a.75.75 0 0 1-.231 1.337 49.948 49.948 0 0 0-9.902 3.912l-.003.002c-.114.06-.227.119-.34.18a.75.75 0 0 1-.707 0A50.88 50.88 0 0 0 7.5 12.173v-.224c0-.131.067-.248.172-.311a54.615 54.615 0 0 1 4.653-2.52.75.75 0 0 0-.65-1.352 56.123 56.123 0 0 0-4.78 2.589 1.858 1.858 0 0 0-.859 1.228 49.803 49.803 0 0 0-4.634-1.527.75.75 0 0 1-.231-1.337A60.653 60.653 0 0 1 11.7 2.805Z" />
         <path d="M13.06 15.473a48.45 48.45 0 0 1 7.666-3.282c.134 1.414.22 2.843.255 4.284a.75.75 0 0 1-.46.711 47.87 47.87 0 0 0-8.105 4.342.75.75 0 0 1-.832 0 47.87 47.87 0 0 0-8.104-4.342.75.75 0 0 1-.461-.71c.035-1.442.121-2.87.255-4.286.921.304 1.83.634 2.726.99v1.27a1.5 1.5 0 0 0-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.66a6.727 6.727 0 0 0 .551-1.607 1.5 1.5 0 0 0 .14-2.67v-.645a48.549 48.549 0 0 1 3.44 1.667 2.25 2.25 0 0 0 2.12 0Z" />
@@ -37,7 +38,7 @@ export function UserCoursesPage() {
         <path d="M11.25 4.533A9.707 9.707 0 0 0 6 3a9.735 9.735 0 0 0-3.25.555.75.75 0 0 0-.5.707v14.25a.75.75 0 0 0 1 .707A8.237 8.237 0 0 1 6 18.75c1.995 0 3.823.707 5.25 1.886V4.533ZM12.75 20.636A8.214 8.214 0 0 1 18 18.75c.966 0 1.89.166 2.75.47a.75.75 0 0 0 1-.708V4.262a.75.75 0 0 0-.5-.707A9.735 9.735 0 0 0 18 3a9.707 9.707 0 0 0-5.25 1.533v16.103Z" />
       </svg>
         All Courses</h2>
-      {listCourses(allCoursesData.data, null, "all-courses")}
+      {listCourses(allCoursesData?.data??[], null, "all-courses")}
 
       {userInfoData.isInstructor &&
         <h2><svg
@@ -50,7 +51,7 @@ export function UserCoursesPage() {
         </svg>Created Courses</h2>
       }
       {userInfoData.isInstructor &&
-        listCourses(myCoursesData.data, 'Create course in instructor profile', "my-courses")
+        listCourses(myCoursesData?.data??[], 'Create course in instructor profile', "my-courses")
       }
 
 
