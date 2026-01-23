8afdd6ee1638378aba9552a88447ab608d00acc6
Leona Marijanović <leona.marijanovic@fer.hr>
2026-01-23 13:00:22 +0100

    Add GlobalError component and error handling

--- frontend/app/pages/lecture/new.tsx
+++ frontend/app/pages/lecture/new.tsx
@@ -6,11 +6,11 @@ import { OpenModal } from "../components/openModal";
 import { QuestionBuilder, type QuestionOptions } from "./questionBuilder";
 import { useEffect, useState } from "react";
 import quiz from "./quiz";
+import { GlobalError } from "../components/GlobalError";
 
 export function NewLecturePage() {
     const fetcher = useFetcher();
-    const { courseInfo, moduleInfo, lectureInfo } = useLoaderData();
-
+    const { courseInfo, moduleInfo, lectureInfo, errorObject } = useLoaderData();
 
     const [questionOptions, setQuestionOption] = useState<QuestionOptions | null>(null);
     const [lastId, setLastId] = useState(0);
@@ -48,6 +48,11 @@ export function NewLecturePage() {
 
     return (
         <section id="content">
+            <GlobalError error={errorObject} />
+            {fetcher.data?.ok === false && (
+                <div className="error">{fetcher.data.errorObject.message}</div>
+            )}
+
             <section id="path">
                 <h4><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
                     <path d="M11.7 2.805a.75.75 0 0 1 .6 0A60.65 60.65 0 0 1 22.83 8.72a.75.75 0 0 1-.231 1.337 49.948 49.948 0 0 0-9.902 3.912l-.003.002c-.114.06-.227.119-.34.18a.75.75 0 0 1-.707 0A50.88 50.88 0 0 0 7.5 12.173v-.224c0-.131.067-.248.172-.311a54.615 54.615 0 0 1 4.653-2.52.75.75 0 0 0-.65-1.352 56.123 56.123 0 0 0-4.78 2.589 1.858 1.858 0 0 0-.859 1.228 49.803 49.803 0 0 0-4.634-1.527.75.75 0 0 1-.231-1.337A60.653 60.653 0 0 1 11.7 2.805Z" />
