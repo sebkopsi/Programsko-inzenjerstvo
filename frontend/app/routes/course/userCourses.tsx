d1de5f6632e04d08cac241a160f7ec01d7d1de3c
Leona Marijanović <leona.marijanovic@fer.hr>
2026-01-23 12:48:32 +0100

    Improve error handling in userCourses fetching
    Refactor user courses fetching logic to handle errors gracefully and return appropriate error messages.

--- frontend/app/routes/course/userCourses.tsx
+++ frontend/app/routes/course/userCourses.tsx
@@ -9,57 +9,101 @@ export async function loader({ request }: Route.LoaderArgs) {
     return redirect("/login");
   }
 
-  const userInfoReq = await fetch("http://localhost:8890/user/my", {
-    method: "GET",
-    headers: {
-      "Authorization": "Bearer " + jwt,
-    },
-  });
+  try {
+    const userInfoReq = await fetch("http://localhost:8890/user/my", {
+      method: "GET",
+      headers: {
+        "Authorization": "Bearer " + jwt,
+      },
+    });
 
 
-  if (!userInfoReq.ok) {
-    throw new Error("Failed to fetch user information");
-  }
-  const userInfoData = await userInfoReq.json();
+    if (!userInfoReq.ok) {
+      return {
+        userInfoData: [],
+        allCoursesData: [],
+        myCoursesData: [],
+        errorObject: { message: "Failed to fetch user info"}
+      };
+    }
+
+    const userInfoData = await userInfoReq.json();
+
+    if (!userInfoData.success) {
+      return {
+        userInfoData: [],
+        allCoursesData: [],
+        myCoursesData: [],
+        errorObject: { message: "Failed to fetch user info"}
+      };
+    }
+
+    const allCoursesReq = await fetch("http://localhost:8890/course/search", {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + jwt,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ term: "", scope: "" })
+    });
+
+    if (!allCoursesReq.ok) {
+      return {
+        userInfoData,
+        allCoursesData: [],
+        myCoursesData: [],
+        errorObject: { message: "Failed to fetch all courses"}
+      };
+    }
+
+    const allCoursesData = await allCoursesReq.json();
+    if (!allCoursesData.success) {
+      return {
+        userInfoData,
+        allCoursesData: [],
+        myCoursesData: [],
+        errorObject: { message: "Failed to fetch all courses"}
+      };
+    }
+
+    const myCoursesReq = await fetch("http://localhost:8890/course/search", {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + jwt,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ term: "", scope: "my" })
+    });
 
 
+    if (!myCoursesReq.ok) {
+      return {
+        userInfoData,
+        allCoursesData,
+        myCoursesData: [],
+        errorObject: { message: "Failed to fetch my courses"}
+      };
+    }
 
-  const allCoursesReq = await fetch("http://localhost:8890/course/search", {
-    method: "POST",
-    headers: {
-      "Authorization": "Bearer " + jwt,
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ term: "", scope: "" })
-  });
+    const myCoursesData = await myCoursesReq.json();
+    if (!myCoursesData.success) {
+      return {
+        userInfoData,
+        allCoursesData,
+        myCoursesData: [],
+        errorObject: { message: "Failed to fetch my courses"}
+      };
+    }
 
-  if (!allCoursesReq.ok) {
-    throw new Error("Failed to fetch all courses");
-  }
+    return {
+      userInfoData,
+      allCoursesData,
+      myCoursesData,
+      errorObject: null
+    };
 
-  const allCoursesData = await allCoursesReq.json();
-
-
-  const myCoursesReq = await fetch("http://localhost:8890/course/search", {
-    method: "POST",
-    headers: {
-      "Authorization": "Bearer " + jwt,
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ term: "", scope: "my" })
-  });
-
-
-  if (!myCoursesReq.ok) {
-    throw new Error("Failed to fetch all courses");
-  }
-  const myCoursesData = await myCoursesReq.json();
-
-
-  return {
-    userInfoData,
-    allCoursesData,
-    myCoursesData
+  } catch (error) {
+    throw new Error("Fetch courses error: " + error);
   }
 }
 
@@ -67,4 +111,3 @@ export async function loader({ request }: Route.LoaderArgs) {
 export default function userCourses() {
   return <UserCoursesPage />
 }
-
