b07ee376c9786f8d4d67bf92f722b7da00333b4c
Leona Marijanović <leona.marijanovic@fer.hr>
2026-01-23 12:59:50 +0100

    Add GlobalError component to LecturePage

--- frontend/app/pages/lecture/lecture.tsx
+++ frontend/app/pages/lecture/lecture.tsx
@@ -5,6 +5,7 @@ import "../styles/lecture.css"
 import ReactMarkdown from "react-markdown";
 import { courseStats } from "../components/courseStats";
 import quiz from "./quiz";
+import { GlobalError } from "../components/GlobalError";
 
 
 const lecturePrepInfo = (data: any) => {
@@ -18,9 +19,11 @@ const lecturePrepInfo = (data: any) => {
 }
 
 export default function LecturePage() {
-  const { courseInfo, moduleInfo, lectureInfo } = useLoaderData();
+  const { courseInfo, moduleInfo, lectureInfo, errorObject } = useLoaderData();
+
   return (
     <section id="content">
+      <GlobalError error={errorObject} />
       <section id="path">
         <h4><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
           <path d="M11.7 2.805a.75.75 0 0 1 .6 0A60.65 60.65 0 0 1 22.83 8.72a.75.75 0 0 1-.231 1.337 49.948 49.948 0 0 0-9.902 3.912l-.003.002c-.114.06-.227.119-.34.18a.75.75 0 0 1-.707 0A50.88 50.88 0 0 0 7.5 12.173v-.224c0-.131.067-.248.172-.311a54.615 54.615 0 0 1 4.653-2.52.75.75 0 0 0-.65-1.352 56.123 56.123 0 0 0-4.78 2.589 1.858 1.858 0 0 0-.859 1.228 49.803 49.803 0 0 0-4.634-1.527.75.75 0 0 1-.231-1.337A60.653 60.653 0 0 1 11.7 2.805Z" />
