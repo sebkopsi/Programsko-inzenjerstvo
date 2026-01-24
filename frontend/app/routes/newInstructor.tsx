79385efa74b3c40287b5fe67eded271510739381
Karlo Klauški <karlo.klauski@tutamail.com>
2026-01-24 19:15:00 +0100

    send form data to backend and redirect to home (temporarily)

--- frontend/app/routes/newInstructor.tsx
+++ frontend/app/routes/newInstructor.tsx
@@ -1,4 +1,21 @@
 import { NewInstructorPage } from "~/pages/instructor/newInstructor";
+import type { Route } from "./+types/newInstructor";
+import { redirect } from "react-router";
+
+export async function action({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+
+  try {
+    const res = await fetch("http://localhost:8890/instructor/promotionRequest", {
+      method: "POST",
+      body: formData
+    });
+  } catch (error: any) {
+    throw new Error(error);
+  }
+
+  return redirect("/");
+}
 
 export default function NewInstructor() {
   return <NewInstructorPage />
